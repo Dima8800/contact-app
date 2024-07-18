dd6a43a34aaf4e67eaf408b8ce9f4412c3d68c60
i22s0623 <i22s0623@students.it-college.ru>
2024-07-18 13:09:55 +0500

    design

--- contact-app/App.js
+++ contact-app/App.js
@@ -1,6 +1,10 @@
-import { View, Text, Button, StyleSheet, FlatList, SafeAreaView} from 'react-native';
+import { View, Text, Button, StyleSheet, FlatList, SafeAreaView, TouchableOpacity, Image} from 'react-native';
 import React, { useEffect, useState } from 'react';
 import Contacts from 'react-native-contacts';
+
+import phoneImage from './assets/phone.png';
+import messageImage from './assets/message.png';
+
 
 
 export default function App() {
@@ -11,7 +15,8 @@ export default function App() {
       <View style={styles.container}>
         <Text style={styles.title}>Контакты</Text>
       </View>
-      <ContacatComponent/>
+      <ContacatComponent name={"Дмитрий"} phoneNumber={"+79220000000"}/>
+      <ContacatComponent name={"Горелов Дмитрий Алекснадрпович"} phoneNumber={"+79220000000"}/>
     </SafeAreaView>
   );
 }
@@ -19,11 +24,25 @@ export default function App() {
 const ContacatComponent = ({name, phoneNumber}) => {
 return(
   <View style={styles.contact}>
-        <Text style={[styles.text]}> Имя контакта: {"\n"} {name}</Text>
-        <Text style={[styles.text]}> Номер контака: {"\n"} {phoneNumber}</Text>
+        <Text style={[styles.text, styles.name]}>{name}</Text>
+        <View style={styles.contactInfo}>
+        <Text style={[styles.text, styles.phone]}>{phoneNumber}</Text>
+        <View style={styles.buttonContainer}>
+        <CustomButton onPress={() => console.log("Button 2 pressed")} imageSource={messageImage} />
+          <CustomButton onPress={() => console.log("Button 2 pressed")} imageSource={phoneImage} />
+        </View>
+      </View>
   </View>
 );
 }
+
+const CustomButton = ({ onPress, imageSource }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Image source={imageSource} style={styles.buttonIcon} />
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   body: {
@@ -32,7 +51,7 @@ const styles = StyleSheet.create({
   },
   container: {
     marginTop: "7%",
-    padding: "1%",
+    padding: "2.5%",
     alignItems: "center",
     justifyContent: 'flex-start',
   },
@@ -43,12 +62,40 @@ const styles = StyleSheet.create({
     color: "#1B1B1B"
  },
  contact: {
-  marginTop: "5%",
-  backgroundColor: "#E1E1E1",
+  borderWidth: 3.5,
+  borderColor: 'blue',
+  borderRadius: 25,
+  marginTop: "2%",
+  margin: "2%",
+  backgroundColor: "#EEEEEE",
   padding: "3%"
  },
  text: {
-  fontSize: 16,
   color: "#1B1B1B"
  },
+ name: {
+  fontSize: 20,
+  fontWeight: "bold"
+ },
+ phone: {
+  fontWeight: "bold"
+ },
+ contactInfo: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+},
+ buttonContainer: {
+  flex: 1,
+  marginLeft: "20%",
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+  marginTop: "3%",
+  alignItems: 'center',
+},
+buttonIcon: {
+  width: 25,
+  height: 25,
+  resizeMode: 'contain',
+},
 });
